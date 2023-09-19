221981683c8f2bd75375a54aba27d6b549cfcb73
Teemu Kalmari <Teemu.Kalmari@student.lab.fi>
2023-09-19 11:58:06 +0300

    Added scroll smoothing with blur and transition delay

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import Animation from './components/borderAnimation.jsx'
+import Scroll from './components/scrollAnimation.jsx'
 import './style/index.css'
 import './style/animation.css'
+import './style/scrollAnimation.css'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Animation/>
+    <Scroll/>
   </React.StrictMode>,
 )

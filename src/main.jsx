c6bac61d080b2d8e6a271804324ab8d3e7b553b3
Teemu Kalmari <Teemu.Kalmari@student.lab.fi>
2023-09-21 16:12:52 +0300

    added wave curve component

--- src/main.jsx
+++ src/main.jsx
@@ -4,16 +4,17 @@ import Border from './components/borderAnimation.jsx'
 import Scroll from './components/scrollAnimation.jsx'
 import Spring from './components/springAnimation.jsx'
 import Lottief from './components/lottieAnimation.jsx'
+import Curve from './components/curveBackground.jsx'
 import './style/index.css'
 
 /*Remember to load the right css fiel for right component*/
-import './style/lottieAnimation.css'
+import './style/curveBackground.css'
 
 
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    < Lottief/>
+    < Curve/>
   </React.StrictMode>,
 )

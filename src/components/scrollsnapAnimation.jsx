09cd5d789489c10d4ece7f787e752cd22570367c
Teemu Kalmari <95919515+temez26@users.noreply.github.com>
2023-09-25 08:23:59 +0300

    added scroll snap component

---
+++ src/components/scrollsnapAnimation.jsx
@@ -0,0 +1,42 @@
+
+
+function Snap() {
+
+
+    return (
+        <div>
+        <nav>Heading</nav>
+        <div>
+            <section>
+                <h2>Page 1</h2>
+
+                <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vehicula erat eu libero viverra, a faucibus nunc ultrices. Integer volutpat risus in augue posuere, vel vestibulum urna pharetra. Suspendisse potenti. In ut scelerisque justo. Quisque ullamcorper eros id magna efficitur cursus. Nullam non lacus at erat congue euismod. Vivamus congue, sapien a posuere hendrerit, purus tellus tincidunt nunc, eget tincidunt purus elit eu tortor. Sed vel mi a orci tincidunt rhoncus. Vivamus sagittis velit ut nunc fringilla, sed rhoncus metus efficitur. Aliquam erat volutpat. Sed ac eleifend justo, id viverra enim. Vivamus bibendum augue vel libero congue, ut bibendum nulla rhoncus. Nulla facilisi. Donec id odio metus. Nulla facilisi. Nulla facilisi. Nulla facilisi.</p>
+            </section>
+            <section>
+                <h2>Page 2</h2>
+
+                <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vehicula erat eu libero viverra, a faucibus nunc ultrices. Integer volutpat risus in augue posuere, vel vestibulum urna pharetra. Suspendisse potenti. In ut scelerisque justo. Quisque ullamcorper eros id magna efficitur cursus. Nullam non lacus at erat congue euismod. Vivamus congue, sapien a posuere hendrerit, purus tellus tincidunt nunc, eget tincidunt purus elit eu tortor. Sed vel mi a orci tincidunt rhoncus. Vivamus sagittis velit ut nunc fringilla, sed rhoncus metus efficitur. Aliquam erat volutpat. Sed ac eleifend justo, id viverra enim. Vivamus bibendum augue vel libero congue, ut bibendum nulla rhoncus. Nulla facilisi. Donec id odio metus. Nulla facilisi. Nulla facilisi. Nulla facilisi.</p>
+            </section>
+            <section>
+                <h2>Page 3</h2>
+
+                <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vehicula erat eu libero viverra, a faucibus nunc ultrices. Integer volutpat risus in augue posuere, vel vestibulum urna pharetra. Suspendisse potenti. In ut scelerisque justo. Quisque ullamcorper eros id magna efficitur cursus. Nullam non lacus at erat congue euismod. Vivamus congue, sapien a posuere hendrerit, purus tellus tincidunt nunc, eget tincidunt purus elit eu tortor. Sed vel mi a orci tincidunt rhoncus. Vivamus sagittis velit ut nunc fringilla, sed rhoncus metus efficitur. Aliquam erat volutpat. Sed ac eleifend justo, id viverra enim. Vivamus bibendum augue vel libero congue, ut bibendum nulla rhoncus. Nulla facilisi. Donec id odio metus. Nulla facilisi. Nulla facilisi. Nulla facilisi.</p>
+            </section>
+            <section>
+                <h2>Page 4</h2>
+
+                <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vehicula erat eu libero viverra, a faucibus nunc ultrices. Integer volutpat risus in augue posuere, vel vestibulum urna pharetra. Suspendisse potenti. In ut scelerisque justo. Quisque ullamcorper eros id magna efficitur cursus. Nullam non lacus at erat congue euismod. Vivamus congue, sapien a posuere hendrerit, purus tellus tincidunt nunc, eget tincidunt purus elit eu tortor. Sed vel mi a orci tincidunt rhoncus. Vivamus sagittis velit ut nunc fringilla, sed rhoncus metus efficitur. Aliquam erat volutpat. Sed ac eleifend justo, id viverra enim. Vivamus bibendum augue vel libero congue, ut bibendum nulla rhoncus. Nulla facilisi. Donec id odio metus. Nulla facilisi. Nulla facilisi. Nulla facilisi.</p>
+            </section>
+            <section>
+                <h2>Page 5</h2>
+
+                <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vehicula erat eu libero viverra, a faucibus nunc ultrices. Integer volutpat risus in augue posuere, vel vestibulum urna pharetra. Suspendisse potenti. In ut scelerisque justo. Quisque ullamcorper eros id magna efficitur cursus. Nullam non lacus at erat congue euismod. Vivamus congue, sapien a posuere hendrerit, purus tellus tincidunt nunc, eget tincidunt purus elit eu tortor. Sed vel mi a orci tincidunt rhoncus. Vivamus sagittis velit ut nunc fringilla, sed rhoncus metus efficitur. Aliquam erat volutpat. Sed ac eleifend justo, id viverra enim. Vivamus bibendum augue vel libero congue, ut bibendum nulla rhoncus. Nulla facilisi. Donec id odio metus. Nulla facilisi. Nulla facilisi. Nulla facilisi.</p>
+            </section>
+        
+        </div>
+        </div >
+
+    )
+}
+
+export default Snap;
